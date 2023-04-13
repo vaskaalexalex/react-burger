12e7dc62d941583f5c2686074ec7d969fe751178
valekhin <v.alehin@istock.info>
2023-04-13 16:01:03 +0300

    review: round 4

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,12 @@
 const BURGER_API_URL = "https://norma.nomoreparties.space/api";
 
+const checkResponse = (res: Response): Promise<any> => {
+  return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
+};
 export async function fetchIngredients() {
   try {
     const response = await fetch(`${BURGER_API_URL}/ingredients`);
-    return await response.json();
+    return await checkResponse(response);
   } catch (error) {
     console.log(error);
   }
